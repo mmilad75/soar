8ed01318fc43024088c36d31222ca2c8866c738b
EminShahbazi <emin.shahbazi@outlook.com>
2024-12-19 09:20:59 +0100

    add sidebar logo

--- src/components/Layout/Sidebar/Sidebar.tsx
+++ src/components/Layout/Sidebar/Sidebar.tsx
@@ -1,5 +1,11 @@
+import SidebarLogo from './SidebarLogo';
+
 const Sidebar: React.FC = () => {
-  return <aside className="w-sidebar border-r border-r-sky">Sidebar</aside>;
+  return (
+    <aside className="w-sidebar border-r border-r-sky">
+      <SidebarLogo />
+    </aside>
+  );
 };
 
 export default Sidebar;
---
+++ src/components/Layout/Sidebar/SidebarLogo.tsx
@@ -0,0 +1,11 @@
+import Logo from '../../../assets/images/logo.png';
+
+const SidebarLogo: React.FC = () => {
+  return (
+    <div className="h-header flex items-center justify-center">
+      <img src={Logo} alt="Soar Logo" />
+    </div>
+  );
+};
+
+export default SidebarLogo;
