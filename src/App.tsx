67a06ac640281203c962119aacb680eae4ac6833
EminShahbazi <emin.shahbazi@outlook.com>
2024-12-19 09:30:27 +0100

    add path alias for src folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router';
-import AppLayout from './components/Layout/AppLayout';
-import Dashboard from './pages/Dashboard';
-import Settings from './pages/Settings';
+import AppLayout from '@/components/Layout/AppLayout';
+import Dashboard from '@/pages/Dashboard';
+import Settings from '@/pages/Settings';
 
 function App() {
   return (
