fb03eb47dfd8940a0bf31b1c8553725b717fa781
EminShahbazi <emin.shahbazi@outlook.com>
2024-12-19 13:42:44 +0100

    implement Icon component

---
+++ src/components/UI/Icon/icons.ts
@@ -0,0 +1,23 @@
+import HomeIcon from '@/assets/icons/home.svg?react';
+import TransferIcon from '@/assets/icons/transfer.svg?react';
+import SettingsIcon from '@/assets/icons/settings.svg?react';
+import UserIcon from '@/assets/icons/user.svg?react';
+import CreditCardIcon from '@/assets/icons/credit-card.svg?react';
+import LoanIcon from '@/assets/icons/loan.svg?react';
+import EconometricsIcon from '@/assets/icons/econometrics.svg?react';
+import InvestmentIcon from '@/assets/icons/investment.svg?react';
+import ServiceIcon from '@/assets/icons/service.svg?react';
+
+export const icons = {
+  creditCard: CreditCardIcon,
+  econometrics: EconometricsIcon,
+  home: HomeIcon,
+  investment: InvestmentIcon,
+  loan: LoanIcon,
+  settings: SettingsIcon,
+  transfer: TransferIcon,
+  user: UserIcon,
+  service: ServiceIcon,
+};
+
+export type IconName = keyof typeof icons;
---
+++ src/components/UI/index.ts
@@ -0,0 +1,3 @@
+export { default as Icon } from './Icon/Icon';
+export { icons } from './Icon/icons';
+export type { IconName } from './Icon/icons';
