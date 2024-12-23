7860497b0ba72563ca1f4f0dca09e3903651d2e0
Milad <mmilad.sanati@gmail.com>
2024-12-24 00:52:17 +0100

    fix page content overflow x issue

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -1,6 +1,6 @@
 const PageContent: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <section className="bg-gray h-content overflow-y-auto">
+    <section className="bg-gray h-content overflow-y-auto overflow-x-hidden">
       <main className=" max-w-screen-xl py-6 px-10 mx-auto">{children}</main>
     </section>
   );
