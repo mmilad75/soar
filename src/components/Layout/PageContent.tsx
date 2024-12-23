6460cad921f21596401ca089081ec039af972cd5
Milad <mmilad.sanati@gmail.com>
2024-12-24 00:52:11 +0100

    fix page content bottom space issue

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -1,7 +1,7 @@
 const PageContent: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <section className="bg-gray overflow-y-auto">
-      <main className="h-content max-w-screen-xl py-6 px-10 mx-auto">{children}</main>
+    <section className="bg-gray h-content overflow-y-auto">
+      <main className=" max-w-screen-xl py-6 px-10 mx-auto">{children}</main>
     </section>
   );
 };
