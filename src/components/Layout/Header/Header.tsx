6a394011ada062b24efbcbc158954eae17a247d8
Milad <mmilad.sanati@gmail.com>
2024-12-24 00:52:05 +0100

    implement page title

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,5 +1,11 @@
+import PageTitle from './PageTitle';
+
 const Header: React.FC = () => {
-  return <header className="flex items-center h-header px-10 border-b border-b-sky">Header</header>;
+  return (
+    <header className="flex items-center justify-between h-header px-10 border-b border-b-sky">
+      <PageTitle />
+    </header>
+  );
 };
 
 export default Header;
