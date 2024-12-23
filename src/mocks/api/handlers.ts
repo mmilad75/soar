628bef595248c652102a1b094d96d91306074d84
Milad <mmilad.sanati@gmail.com>
2024-12-24 00:52:07 +0100

    integrate redux toolkit and implement cards widget

--- src/mocks/api/handlers.ts
+++ src/mocks/api/handlers.ts
@@ -1,10 +1,18 @@
 import { http, HttpResponse } from 'msw';
+import { cards } from './data/cards';
 
 export const handlers = [
-  http.get('https://soar-api/credit-cards', () => {
+  http.get('https://soar-api/credit-cards', ({ request }) => {
+    const params = new URL(request.url).searchParams;
+    const start = Number(params.get('start'));
+    const limit = Number(params.get('limit'));
+
     return HttpResponse.json(
       {
-        cards: [],
+        data: cards.slice(start, start + limit),
+        meta: {
+          total: cards.length,
+        },
       },
       {
         status: 200,
--- src/store/slices/creditCardsSlice.ts
+++ src/store/slices/creditCardsSlice.ts
@@ -1,24 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
+import cardsApi, { ICreditCard } from '@/api/cards';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-interface ICreditCard {
-  cardNumber: string;
-  fullName: string;
-  expirationDate: string;
-  balance: number;
-}
+export const fetchCards = createAsyncThunk(
+  'creditCards/fetchCards',
+  async ({ start, limit }: { start: number; limit: number }) => {
+    const { cards, meta } = await cardsApi.fetchCards(start, limit);
+
+    return {
+      cards,
+      meta,
+    };
+  }
+);
 
 export interface ICreditCardsState {
   cards: ICreditCard[];
+  total: number;
 }
 
 const initialState: ICreditCardsState = {
   cards: [],
+  total: 0,
 };
 
 export const creditCardsSlice = createSlice({
   name: 'creditCards',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchCards.fulfilled, (state, action) => {
+      state.cards = action.payload.cards;
+      state.total = action.payload.meta.total;
+    });
+  },
 });
 
 export default creditCardsSlice.reducer;
