01453a0fd0d369a4ccfe7929d091b2529a556365
Milad <mmilad.sanati@gmail.com>
2024-12-24 00:52:09 +0100

    implement transactions store and api

---
+++ src/mocks/api/data/transactions.ts
@@ -0,0 +1,26 @@
+export const transactions: ITransaction[] = [
+  {
+    id: 't1',
+    amount: 850,
+    date: '2021-01-28',
+    source: 'card',
+    title: 'Deposit from my Card',
+    type: 'withdrawal',
+  },
+  {
+    id: 't2',
+    amount: 2500,
+    date: '2021-01-25',
+    source: 'paypal',
+    title: 'Deposit Paypal',
+    type: 'deposit',
+  },
+  {
+    id: 't3',
+    amount: 5400,
+    date: '2021-01-21',
+    source: 'cash',
+    title: 'Jemi Wilson',
+    type: 'deposit',
+  },
+];
--- src/mocks/api/handlers.ts
+++ src/mocks/api/handlers.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse } from 'msw';
 import { cards } from './data/cards';
+import { transactions } from './data/transactions';
 
 export const handlers = [
   http.get('https://soar-api/credit-cards', ({ request }) => {
@@ -10,6 +11,23 @@ export const handlers = [
     return HttpResponse.json(
       {
         data: cards.slice(start, start + limit),
+        meta: {
+          total: cards.length,
+        },
+      },
+      {
+        status: 200,
+      }
+    );
+  }),
+  http.get('https://soar-api/transactions', ({ request }) => {
+    const params = new URL(request.url).searchParams;
+    const start = Number(params.get('start'));
+    const limit = Number(params.get('limit'));
+
+    return HttpResponse.json(
+      {
+        data: transactions.slice(start, start + limit),
         meta: {
           total: cards.length,
         },
